16fea3ead5fa52dd3e76880276bac24822ae8131
Danny944 <fadehandaniel2006@gmail.com>
2024-10-29 10:38:51 +0100

    changed back

--- front_end/vite.config.ts
+++ front_end/vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/News-Aggregator/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
